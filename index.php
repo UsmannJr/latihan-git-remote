1a9b9bc77376cca169b6081eb7b869d64d08145a
Usman <usmanjr100@gmail.com>
2024-01-17 23:16:52 +0700

    Menambahkan paragraf lorem

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Git remote Local To Github</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores hic delectus earum neque aspernatur veritatis dolore quia sit voluptate, totam atque, possimus eos ab? Inventore pariatur facilis explicabo incidunt omnis.</p>
 </body>
 </html>
