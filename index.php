8a432fa4e3a771c82ca7302258a3f97c2c79b5d0
Usman <usmanjr100@gmail.com>
2024-01-21 10:57:45 +0700

    Menambahkan class div dan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Git remote Local To Github</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores hic delectus earum neque aspernatur veritatis dolore quia sit voluptate, totam atque, possimus eos ab? Inventore pariatur facilis explicabo incidunt omnis.</p>
+    <div class="container">
+        <div class="lorem">
+            <p>lorem</p>
+        </div>
+    </div>
 </body>
 </html>
